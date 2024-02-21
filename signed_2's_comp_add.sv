f1edd50f8c6e51b0263b4f31f72c79ea808bddc9
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-02-22 00:53:01 +0500

    updated

---
+++ signed_2's_comp_add.sv
@@ -0,0 +1,28 @@
+// signed 2's complement addition
+// if num1 >0 and num2>0 then add normally as usual
+// if num<0 and num2<0 then again add them as usual
+// if num1>0 and num2<0 then take 2'scomplement of num2 and add it into num1
+// if num1<0 and num2>0 then take 2'scomplement of num1 and add it into num2
+
+module signed_2's_comp_add (
+    input logic [31:0] num1,
+    input logic [31:0] num2,
+    output logic [31:0] s_add
+);
+
+if ((num1 >0 & num2>0) | (num1<0 & num2<0) ) begin
+    s_add = num1 + num2
+end
+
+else if (num1 >0 & num2<0) begin
+    num2 = !num2 + 1;
+    s_add = num1 +num2;
+end
+
+else if (num1<0 & num2>0) begin
+    num1 = !num1 +1;
+    s_add = num1+num2;
+end
+
+    
+endmodule
