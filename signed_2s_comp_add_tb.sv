a7f4a9e79f171dd28d835c3fa70157c165629c81
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-02-22 02:04:33 +0500

    updated

--- signed_2s_comp_add_tb.sv
+++ signed_2s_comp_add_tb.sv
@@ -4,6 +4,7 @@ logic [31:0] num_1;
 logic [31:0] num_2;
 logic [31:0] s_add1;
 
+
 localparam period = 10;
 
 signed_2s_comp_add UUT(
@@ -15,19 +16,19 @@ signed_2s_comp_add UUT(
 initial begin
     num_1 = 32'd1010; num_2 = 32'd1000;
     #period;
-    num_1 = -32'd1001253; num_2 = -32'd263784;
+    num_1 = -32'h10002530; num_2 = -32'h12637841;
     #period;
-    num_1 = 32'd263; num_2 = -32'd27383;
+    num_1 = 32'd263; num_2 = -32'h10027383;
     #period;
-    num_1 = -32'd2526393; num_2 = 32'd5363;
+    num_1 = -32'h10226393; num_2 = 32'd5363;
     #period;
-    num_1 = -32'd263; num_2 = 32'd27383;
+    num_1 = -32'h10000263; num_2 = 32'd27383;
     #period;
-    num_1 = 32'd2526393; num_2 = -32'd5363;
+    num_1 = 32'd2526393; num_2 = -32'h10005363;
     #period;
-    num_1 = -32'd263233; num_2 = 32'd27383;
+    num_1 = -32'h10263233; num_2 = 32'd27383;
     #period;
-    num_1 = 32'd25263; num_2 = -32'd536336;
+    num_1 = 32'd25263; num_2 = -32'h10536336;
     #period;
     $stop;
 end
