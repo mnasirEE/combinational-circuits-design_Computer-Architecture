e48a8e576f4244677fb17f1ac62e9e572f0dcc81
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-02-23 21:46:36 +0500

    updated

--- signed_2s_comp_add_tb.sv
+++ signed_2s_comp_add_tb.sv
@@ -14,21 +14,13 @@ signed_2s_comp_add UUT(
 );
 
 initial begin
-    num_1 = 32'd1010; num_2 = 32'd1000;
+    num_1 = 32'd10; num_2 = 32'd3;
     #period;
-    num_1 = -32'h10002530; num_2 = -32'h12637841;
+    num_1 = -32'd5; num_2 = -32'd3;
     #period;
-    num_1 = 32'd263; num_2 = -32'h10027383;
+    num_1 = 32'd2; num_2 = -32'd7;
     #period;
-    num_1 = -32'h10226393; num_2 = 32'd5363;
-    #period;
-    num_1 = -32'h10000263; num_2 = 32'd27383;
-    #period;
-    num_1 = 32'd2526393; num_2 = -32'h10005363;
-    #period;
-    num_1 = -32'h10263233; num_2 = 32'd27383;
-    #period;
-    num_1 = 32'd25263; num_2 = -32'h10536336;
+    num_1 = -32'd15; num_2 = 32'd21;
     #period;
     $stop;
 end
